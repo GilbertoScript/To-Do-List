c1c041dff82115552ffd8e52465e93b37ad29c61
Gilberto Alves <56325350+GilbertoASJ@users.noreply.github.com>
2021-01-05 17:13:58 -0300

    Add file via upload

---
+++ js/scripts.js
@@ -0,0 +1,46 @@
+var ul = document.getElementById("myList")
+var li
+var itemId
+var item
+
+function addTask() {
+    if(document.getElementById("task").value != ''){
+        item = document.getElementById("task")
+        itemId = ul.childElementCount
+
+        li = createItemEl(item.value, itemId)
+        li.appendChild(createRemoveTaskBtn(itemId))
+        ul.appendChild(li)
+        item.value = ''
+    }
+
+}
+
+function removeTask(itemId) {
+    for( i = 0 ; i < ul.children.length; i++ ) {
+        if(ul.children[i].getAttribute("index") == itemId) {
+            ul.children[i].remove()
+        }
+    }
+
+}
+
+function createItemEl(itemValue, itemId) {
+    let li = document.createElement("li")
+
+    li.setAttribute("index", itemId)
+    li.appendChild(document.createTextNode(itemValue))
+
+    return li
+
+}
+
+function createRemoveTaskBtn(itemId) {
+    let btn = document.createElement("button")
+
+    btn.setAttribute("onclick", "removeTask("+itemId+")")
+    btn.innerHTML = 'X'
+
+    return btn
+
+}
