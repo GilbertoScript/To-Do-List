e9761ad6f8c68fbb60bb1471a48cc4975af6f9d0
Gilberto Alves <56325350+GilbertoASJ@users.noreply.github.com>
2021-01-07 16:57:22 -0300

    Add file via upload

--- js/scripts.js
+++ js/scripts.js
@@ -1,46 +1,46 @@
-var ul = document.getElementById("myList")
-var li
-var itemId
-var item
+var ul = document.getElementById("myList");
+var li;
+var itemId;
+var item;
 
 function addTask() {
-    if(document.getElementById("task").value != ''){
-        item = document.getElementById("task")
-        itemId = ul.childElementCount
+  if (document.getElementById("task").value != "") {
+    item = document.getElementById("task");
+    itemId = ul.childElementCount;
 
-        li = createItemEl(item.value, itemId)
-        li.appendChild(createRemoveTaskBtn(itemId))
-        ul.appendChild(li)
-        item.value = ''
-    }
-
+    li = createItemEl(item.value, itemId);
+    li.appendChild(createRemoveTaskBtn(itemId));
+    ul.appendChild(li);
+    item.value = "";
+  }
 }
 
 function removeTask(itemId) {
-    for( i = 0 ; i < ul.children.length; i++ ) {
-        if(ul.children[i].getAttribute("index") == itemId) {
-            ul.children[i].remove()
-        }
+  for (i = 0; i < ul.children.length; i++) {
+    if (ul.children[i].getAttribute("index") == itemId) {
+      ul.children[i].remove();
     }
-
+  }
 }
 
 function createItemEl(itemValue, itemId) {
-    let li = document.createElement("li")
+  let li = document.createElement("li");
 
-    li.setAttribute("index", itemId)
-    li.appendChild(document.createTextNode(itemValue))
+  li.setAttribute("index", itemId);
+  li.appendChild(document.createTextNode(itemValue));
 
-    return li
-
+  return li;
 }
 
 function createRemoveTaskBtn(itemId) {
-    let btn = document.createElement("button")
+  let btn = document.createElement("button");
 
-    btn.setAttribute("onclick", "removeTask("+itemId+")")
-    btn.innerHTML = 'X'
+  btn.setAttribute("onclick", "removeTask(" + itemId + ")");
+  btn.innerHTML = "X";
 
-    return btn
+  return btn;
+}
 
+function campoFoco() {
+  document.getElementById("task").style.backgroundColor = "whitesmoke";
 }
